a129fa12dbeda7de21006732124eef9c8de73cde
Crescent Clinic <123446403+crescentclinic@users.noreply.github.com>
2023-04-28 01:44:30 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,10 +4,17 @@ console.log(datetime);
 
 function refreshTime() {
   const timeDisplay = document.getElementById("time");
-  const dateString = new Date().toLocaleString('en-us', {weekday:"long", day:"numeric", month:"short", year:"numeric", hour:"numeric", minute:"numeric", second:"numeric"});
+  const dateString = new Date().toLocaleString('en-us', { weekday: "long", day: "numeric", month: "short", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric" });
   timeDisplay.textContent = dateString;
 }
-  setInterval(refreshTime, 1000);
+setInterval(refreshTime, 1000);
+
+
+function copy() {
+  let textarea = document.getElementById("input");
+  textarea.select();
+  document.execCommand("copy");
+}
 
 function openTab(evt, tabName) {
   // Declare all variables
@@ -15,16 +22,16 @@ function openTab(evt, tabName) {
 
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
-   for (i = 0; i < tabcontent.length; i++) {
-     tabcontent[i].style.display = "none";
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-  
+
   // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
-   for (i = 0; i < tablinks.length; i++) {
-  tablinks[i].className = tablinks[i].className.replace(" active", "");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  
+
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
